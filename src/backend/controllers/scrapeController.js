863e5788df4bc687fe2505ace59c0bc6c44cdf24
Meraduro <andy.hoogvorst@gmail.com>
2023-10-13 15:26:50 -0400

    Formatted the raw data on the back end, parsing on the front end

--- src/backend/controllers/scrapeController.js
+++ src/backend/controllers/scrapeController.js
@@ -1,9 +1,28 @@
 // import readline from "readline";
 
+import { json } from "express";
+
 function scrapeController(response) {
   let textData = response.data;
-  const lines = textData.split("\n");
-  return lines[2];
+  var lines = (textData.split("\n"));
+  var headers = lines[0]
+  headers = headers.split(/\s+/);
+  headers[0] = "YY"
+  var jsonObj = []
+  console.log(headers)
+  for (var i = 1; i < lines.length; i++) {
+    var data = lines[i].split(/\s+/);
+    var obj = {};
+    for (var j = 0; j < data.length; j++) {
+      obj[headers[j].trim()] = data[j].trim();
+    }
+    jsonObj.push(obj);
+  }
+  console.log(jsonObj[10].YY)
+  JSON.stringify(jsonObj);
+  return jsonObj;
+
+
 }
 
 export default scrapeController;
--- src/frontend/components/Home.jsx
+++ src/frontend/components/Home.jsx
@@ -7,18 +7,30 @@ function Home() {
 
   function getData() {
     axios.get("http://localhost:8000/api/scrape", { crossdomain: true }).then((res) => {
-      setData(res.data);
+      console.log("andy", res.data)
+      var final = dataFormatter(res.data)
+      setData(final);
     }).catch(error => {
       console.log(error);
     })
   }
 
+  function dataFormatter(data) {
+    console.log("hi")
+    var toReturn = ""
+    for (let i = 1; i < 10; i++) {
+      toReturn = toReturn.concat(JSON.stringify(data[i]));
+      console.log(data[i]);
+    }
+    console.log('andy is aweswome', toReturn)
+    return toReturn
+  }
 
   return (
     <div>
       <h1>Home Page</h1>
       <button onClick={getData}>
-        Click Me!
+        Last 5 hours
       </button>
       <button className="clear" onClick={() => setData("")}>
         Clear Data
